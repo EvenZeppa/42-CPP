d67635cf5a9b5a4380a5dbba657c64b6dca912f4
ezeppa <ezeppa@Debian.myguest.virtualbox.org>
2024-12-27 12:44:00 +0100

    END CPP02 ex01

--- CPP02/ex01/Fixed.cpp
+++ CPP02/ex01/Fixed.cpp
@@ -14,12 +14,30 @@ Fixed::Fixed(const Fixed &fixed) : _value(fixed._value)
 
 Fixed::Fixed(int value)
 {
-	this->_value = (value << this->_bits);
+	this->_value = value;
+	this->_value <<= this->_bits;
 }
 
 Fixed::Fixed(float value)
 {
-	this->_value = value * (1 << this->_bits);
+	int		integer_part = roundf(value);
+	float	decimal_part;
+	int		sizeDecimal = 1;
+	int		final;
+
+	if (integer_part == roundf(value + 0.5))
+		integer_part--;
+	decimal_part = value - integer_part;
+	
+	this->_value = integer_part;
+	this->_value <<= this->_bits;
+	
+	for (int i = ((1 << this->_bits) - 1); i >= 10; i /= 10)
+		sizeDecimal *= 10;
+
+	final = roundf(decimal_part * sizeDecimal);
+
+	this->_value += final;	
 }
 
 Fixed& Fixed::operator=(const Fixed &fixed)
@@ -31,7 +49,10 @@ Fixed& Fixed::operator=(const Fixed &fixed)
 
 std::ostream& operator<<(std::ostream &os, const Fixed &fixed)
 {
-	os << fixed.toFloat();
+	if (fixed._value & ((1 << fixed._bits) - 1))
+		os << fixed.toFloat();
+	else
+		os << fixed.toInt();
 	return os;
 }
 
@@ -59,5 +80,15 @@ int Fixed::toInt(void) const
 
 float Fixed::toFloat(void) const
 {
-	return static_cast<float>(this->_value) / (1 << this->_bits);
+	int integer_part = this->_value >> this->_bits;
+	int decimal_part = this->_value & ((1 << this->_bits) - 1);
+	int sizeDecimal = 1;
+
+	if (decimal_part == 0)
+		return (integer_part);
+	
+	for (int i = ((1 << this->_bits) - 1); i >= 10; i /= 10)
+		sizeDecimal *= 10;
+
+	return (static_cast<float>(integer_part) + static_cast<float>(decimal_part) / sizeDecimal);
 }
