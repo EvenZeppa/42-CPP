60e8cc12c2179fb447f971ea92f7a9b5f7bf599e
ezeppa <zeppaeven.dev@gmail.com>
2025-04-23 09:08:49 +0200

    UPD AForm

---
+++ CPP05/ex02/AForm.hpp
@@ -0,0 +1,51 @@
+#ifndef FORM_HPP
+# define FORM_HPP
+
+# include <iostream>
+# include <string>
+# include <exception>
+# include "Bureaucrat.hpp"
+
+class AForm {
+public:
+	AForm(std::string name, int signGrade, int execGrade);
+	virtual ~AForm();
+	AForm(const AForm& other);
+	AForm& operator=(const AForm& other);
+
+	std::string getName() const;
+	bool isSigned() const;
+	int getSignGrade() const;
+	int getExecGrade() const;
+
+	void beSigned(const Bureaucrat& bureaucrat);
+	virtual void execute(const Bureaucrat& executor) const = 0;
+
+	class GradeTooHighException : public std::exception {
+		public:
+			const char* what() const throw();
+	};
+
+	class GradeTooLowException : public std::exception {
+		public:
+			const char* what() const throw();
+	};
+
+	class FormNotSignedException : public std::exception {
+		public:
+			const char* what() const throw();
+	};
+
+	class FormAlreadySignedException : public std::exception {
+		public:
+			const char* what() const throw();
+	};
+private:
+	const std::string _name;
+	bool _isSigned;
+	const int _signGrade;
+	const int _execGrade;
+};
+std::ostream& operator<<(std::ostream& os, const AForm& form);
+
+#endif
